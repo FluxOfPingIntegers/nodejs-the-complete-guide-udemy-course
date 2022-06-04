798249b761c2d059ec42310e42c111fbf2cd82f5
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-06-04 18:03:57 -0400

    Node Lifecycle & Event Loop - completed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
-const server = http.createServer((req, rest) => {
+const server = http.createServer((req, res) => {
   console.log(req);
+  // process.exit();
 });
 
 server.listen(3000);
