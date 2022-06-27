5d4d4cda120b3c20998982f24d335b07c747210b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-06-27 10:02:53 -0400

    Adding the Layout to Handlebars - completed

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({
+  layoutsDir: 'views/layouts/', 
+  defaultLayout: 'main-layout', 
+  extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { addAbortSignal } = require('stream');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
