2bae4885804ec788bf98a1cb2ac7b1687ee35922
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-08 14:38:57 -0400

    Installing the MongoDB Driver - completed

--- util/database.js
+++ util/database.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
-const password = process.env.CASUAL_PASS;
+const password = process.env.OLD_PASS;
 
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', password, {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://RSchleck:${password}@cluster0.dyucjhi.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(result => {
+      console.log('Connected!')
+      callback(client);
+    })
+    .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
