2728ae3a567e96d5b5d705996ca05ef8fdd60b5d
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-09 14:08:15 -0400

    Finishing the Database Connection - completed

--- util/database.js
+++ util/database.js
@@ -4,15 +4,29 @@ const password = process.env.OLD_PASS;
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    `mongodb+srv://RSchleck:${password}@cluster0.dyucjhi.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://RSchleck:${password}@cluster0.dyucjhi.mongodb.net/shop?retryWrites=true&w=majority`
     )
-    .then(result => {
-      console.log('Connected!')
-      callback(result);
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
