346a71cb069b0d6a1e873636575e3ad2116b10fc
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-10 12:39:28 -0400

    Fetching a Single Product - completed

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -10,7 +11,8 @@ class Product {
 
   save() {
     const db = getDb();
-    db.collection('products').insertOne(this)
+    return db
+    .collection('products').insertOne(this)
       .then(result => {
         console.log(result);
       })
@@ -18,6 +20,7 @@ class Product {
         console.log(err);
       });
   }
+
   static fetchAll() {
     const db = getDb();
     return db
@@ -27,6 +30,21 @@ class Product {
       .then(products => {
         console.log(products);
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
       })
       .catch(err => {
         console.log(err);
