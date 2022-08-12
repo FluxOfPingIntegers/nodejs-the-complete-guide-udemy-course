9de08fbf9bf46f0e059e84d17bdd3d2f4d1fc52d
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-12 16:16:07 -0400

    Adding the 'Add to Cart' Functionality - completed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('62f54b08467e535061c3aec9')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
