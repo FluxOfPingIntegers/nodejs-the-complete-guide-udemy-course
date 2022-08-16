80cd9b5b1ba7f2aa2ef33ac00c0ec78a07196a35
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-16 08:20:18 -0400

    Getting Orders - completed

--- models/user.js
+++ models/user.js
@@ -104,8 +104,10 @@ class User {
   }
 
   getOrders() {
-    const db = getDB();
-    // return db.collection('orders')
+    const db = getDb();
+    return db.collection('orders')
+      .find({'user._id': new ObjectId(this._id)})
+      .toArray();
   }
 
   static findById(userId) {
