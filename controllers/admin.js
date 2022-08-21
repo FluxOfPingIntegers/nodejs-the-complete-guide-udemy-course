1bc0877a001d9c85a5eb9248dc0dce385d0afff7
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-21 07:29:05 -0400

    One Important Thing About Fetching Relations - completed

--- controllers/admin.js
+++ controllers/admin.js
@@ -74,7 +74,10 @@ exports.getAddProduct = (req, res, next) => {
 
   exports.getProducts = (req, res, next) => {
     Product.find()
+      // .select('title price -_id')
+      // .populate('userId', 'name')
       .then(products => {
+        console.log(products);
         res.render('admin/products', {
           prods: products, 
           pageTitle: 'Admin Products', 
