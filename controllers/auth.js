ec8006c5b26f588823e53eed0dcc49657d1b664b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-30 11:27:57 -0400

    Assignment 5: Time to Practice - Sessions and Cookies - completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,19 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req
-  //   .get('Cookie')
-  //   .split(';')[1]
-  //   .trim()
-  //   .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
-  res.render('auth/orders', {
+  res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    orders: orders,
     isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLogginIn = true;
-  res.render('/');
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(() => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.render('/');
+    })
+  .catch(err => console.log(err))
 };
