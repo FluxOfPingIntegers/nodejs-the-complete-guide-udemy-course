c834fbbc6ed6e52951d1dc71b51c17afcb208b1d
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-08-31 13:35:41 -0400

    Making the 'Add to Cart' Work Again - completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,16 +10,16 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   User.findById('6300f91ac067c4354358d600')
-    .then(() => {
+    .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       res.render('/');
     })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
+  req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
