259f4bdbd68072789845ca0decfa80a06f06b9ef
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-10-22 10:48:46 -0400

    Adding a Create Post Endpoint - completed

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -5,7 +5,7 @@ exports.getPosts = (req, res, next) => {
         _id: 1,
         title: 'First Post', 
         content: 'This is the first post!', 
-        imageUrl: 'images/duck.jpg', 
+        imageUrl: /* 'images/duck.jpg' */ '', 
         creator: {
           name: 'Maximilian'
         },
@@ -21,6 +21,12 @@ exports.createPost = (req, res, next) => {
     // Create post in db
     res.status(201).json({
         message: 'Post created successfully!',
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: {
+          _id: new Date().toISOString(), 
+          title: title, 
+          content: content, 
+          creator: { name: "Ryan"},
+          createdAt: new Date()
+        }
     });
 };
