5f813503ad01fd876ee5baff2ee75cabd64268eb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-10-24 07:21:29 -0400

    Storing Posts in the Database - completed

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.json({
     posts: [
@@ -30,14 +32,20 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     // Create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-          _id: new Date().toISOString(), 
-          title: title, 
-          content: content, 
-          creator: { name: "Ryan" },
-          createdAt: new Date()
-        }
+    const post = new Post({
+      title: title, 
+      content: content, 
+      imageUrl: /* 'images/duck.jpg'  */ '',
+      creator: { name: "Ryan" },
     });
+    post.save().then(result => {
+      console.log(result);
+      res.status(201).json({
+          message: 'Post created successfully!',
+          post: result
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 };
