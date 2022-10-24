f7d5fc0de134ecb57f52299127277765dd2846fa
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-10-24 07:30:05 -0400

    Static Images & Error Handling - completed

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,7 +23,14 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-mongoose.connect(mongoURI)
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({message: message});
+})
+
+mongoose.connect(mongoURL)
   .then(result => {
     app.listen(8080);
   })
