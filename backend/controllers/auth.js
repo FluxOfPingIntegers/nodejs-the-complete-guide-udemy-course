3c982db97f18237d5e003ddc9c76f83fc99cf664
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-11-07 09:21:38 -0500

    Assignment 7: Working with REST APIs - completed

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -70,4 +70,45 @@ exports.login = (req, res, next) => {
         err.statusCode = 500;
       }
     })
-}
+};
+
+module.exports.getUserStatus = (req, res, next) => {
+  User.findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw err;
+      }
+      res.status(200).json({status: user.status});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+};
+
+module.exports.updateUserStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'User updated.'});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
