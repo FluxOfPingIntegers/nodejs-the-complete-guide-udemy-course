3006fbeb2f65c137813b7c4767c8b492a29766eb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-11-25 13:27:17 -0500

    Uploading Images - completed

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const express = require('express');
@@ -58,6 +59,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({ message: 'File stored.', filePath: req.file.path });
+});
+
+app.use(auth);
+
 app.use('/graphql', 
   graphqlHttp({
     schema: graphqlSchema,
@@ -87,3 +103,8 @@ mongoose.connect(mongoUrlPrefix + oldPass + mongoUrlSuffix)
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+  const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+  };
