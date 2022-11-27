8f9b02bbe0a279e8c557154969c931e35557e51d
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-11-27 12:32:44 -0500

    Adding Posts & Pagination - completed

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 require('dotenv').config();
 
 const express = require('express');
@@ -14,6 +13,7 @@ const graphqlHttp = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const cors = require('cors');
 const app = express();
@@ -103,8 +103,3 @@ mongoose.connect(mongoUrlPrefix + oldPass + mongoUrlSuffix)
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
-  const clearImage = filePath => {
-    filePath = path.join(__dirname, '..', filePath);
-    fs.unlink(filePath, err => console.log(err));
-  };
