3b5c51dfbdc9614d5c50baf26e33e9f7272795a4
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-12-06 17:29:37 -0500

    Setting Up a SSL Server - completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 require('dotenv').config();
 const mongo_user = process.env.MONGO_USER;
 const password = process.env.OLD_PASS;
@@ -29,6 +30,9 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -126,7 +130,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(process.env.PORT || 3000);
+    // https
+    //   .createServer({key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 3000);
+      app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
